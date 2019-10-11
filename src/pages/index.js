d8cdc2f7e224492d16996ef204eb010e06b17f79
martinpsz <martinpsz@protonmail.com>
2019-10-11 11:10:00 -0400

    Adding fonts and bootstrap support

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
-import React from "react"
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css'
 
-export default () => <div>Coming soon!</div>
+export default () => <div >
+    <h1>Testing</h1>
+    <p>Testing 123</p>
+</div>
---
+++ src/utils/typography.js
@@ -0,0 +1,17 @@
+import Typography from 'typography';
+
+const typography = new Typography({
+    baseFontSize: "16px",
+    baseLineHeight: 1.666,
+    headerFontFamily: [
+        "Playfair Display",
+        "serif"
+    ],
+    headerWeight: 900,
+    bodyFontFamily: [
+        "Raleway",
+        "sans-serif"
+    ]
+})
+
+export default typography;
