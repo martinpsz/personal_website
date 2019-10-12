4af097ff3b08497d5e8f36ae335a0789c2ad6b79
martinpsz <martinpsz@protonmail.com>
2019-10-12 18:43:18 -0400

    Trying to set up dev branch deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default () => {
         justifyContent: 'center',
         alignItems: 'center',
         height: '100vh',
-        background: 'var(--Orange)',
+        background: 'var(--Blue)',
         color: 'var(--White)',
     }
 
